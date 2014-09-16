d5f4b360561575bc6461a90d1fbae806a1014a8d
Tom@Pegasus <info@thomas-mundt.de>
2014-09-16 11:02:13 +0200

    exercise 6 finished

--- core/learnyounode/06-make-it-modular.js
+++ core/learnyounode/06-make-it-modular.js
@@ -1,4 +1,9 @@
-var mymodule = require('./mymodule.js')
+var mymodule = require('./06-mymodule.js');
 
-
+mymodule(process.argv[2], process.argv[3], function(err, data){
+	//console.log(data);
+	for ( result in data) {
+		console.log(data[result]);
+	}	
+});
 
--- core/learnyounode/06-mymodule.js
+++ core/learnyounode/06-mymodule.js
@@ -1,3 +1,28 @@
-module.exports = function (args) {
-	
+module.exports = function (dirPath, fileExt, callback) {
+	var fs = require('fs');
+	var path = require('path');
+
+	//console.log("Module args: " + dirPath + " "+fileExt+" "+callback);
+	//console.log(process.argv);
+	var results = [];
+	//console.log(process.argv);
+	var buffer = fs.readdir(dirPath, function (error, list) {
+		//function foo(error, list) {
+			if (error) {
+				return callback(error);
+			}
+			if (!error) {
+				//console.log(data);
+				list.forEach(function(file){
+					if (path.extname(file) == "."+ fileExt) {
+						//console.log("File found, name: " + file);
+						results.push(file);
+					}
+
+				});
+				
+			}
+			return callback(null, results);
+		//}
+	});
 }
