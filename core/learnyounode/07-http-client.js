d486d2ed273e538461212518d3d59c39efb48538
Tom@Pegasus <info@thomas-mundt.de>
2014-09-17 18:33:46 +0200

    excercise #7 completed

---
+++ core/learnyounode/07-http-client.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+//console.log(process.argv);
+http.get(process.argv[2], function (response) {
+	response.setEncoding('utf8');
+	response.on('data', function(data) {
+		console.log(data);
+	})
+});
