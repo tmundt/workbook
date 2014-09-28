00d22dc6b5e345f77f28bd0a6ac16e1797cdc39c
Tom@Pegasus <info@thomas-mundt.de>
2014-09-28 20:02:15 +0200

    excercise #9 completed

--- core/learnyounode/09-juggling-async.js
+++ core/learnyounode/09-juggling-async.js
@@ -2,47 +2,51 @@ var http = require('http');
 
 //console.log(process.argv);
 
+// save three URLs to the server
+var serverURLList = [];
+for (var i = 2; i<process.argv.length;i++) {
+	//console.log('URL Nr. '+i+': '+ process.argv[i]);
+	serverURLList.push(process.argv[i]);
+}
+
 var responseStringURL1 = "";
 var responseStringURL2 = "";
 var responseStringURL3 = "";
 var counterFinishedRequests = 0;
 
-for (var i=2; i < 5;i++){
-	http.get(process.argv[i], function (response) {
-		response.setEncoding('utf8');
-		response.on('data', function(data) {
-			//console.log(data);
-			switch(i) {
-				case "2" : responseStringURL1 = responseStringURL1 + data;
-				console.log("case 2 detected");
+
+var getDataFromURL = function(url) {
+	http.get(serverURLList[url], function(response) {
+		response.on('data', function(data){
+			// console.log('getDataFromURL, getting data');
+			switch(url) {
+				case 0:
+				responseStringURL1 += data;
 				break;
-				case 3 : responseStringURL2 = responseStringURL2 + data;
+				case 1:
+				//console.log('url ist: ' + url);
+				responseStringURL2 += data;
 				break;
-				case 4 : responseStringURL3 = responseStringURL3 + data;
+				case 2:
+				responseStringURL3 += data;
+				//console.log('url ist: ' + url);
 				break;
 			}
+				
 		})
-		response.on('end', function() {
-
-			//console.log(responseString.length);
-			//console.log(responseStringURL1);
-			endedRequest(i);
-		})
+		response.on('end', function(){
+			//console.log("response at an end for url 2");
+			counterFinishedRequests +=1;
+			//console.log(counterFinishedRequests);
+			if(counterFinishedRequests < 3) {
+				getDataFromURL(counterFinishedRequests);
+			} else if (counterFinishedRequests <= 3) {
+				console.log(responseStringURL1);
+				console.log(responseStringURL2);
+				console.log(responseStringURL3);
+			};
+			
+		});
 	});
 }
-
-
-var endedRequest = function(urlNumber) {	
-	if (urlNumber) {
-		counterFinishedRequests +=1;
-	}
-	console.log("endedRequest:"+ counterFinishedRequests);
-	console.log(responseStringURL1);
-	if(counterFinishedRequests == 3) {
-		console.log(responseStringURL1);
-		console.log(responseStringURL2);
-		console.log(responseStringURL3);
-	}
-	
-
-}
+getDataFromURL(counterFinishedRequests);
