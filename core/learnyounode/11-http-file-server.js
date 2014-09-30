626c8165e33a54c16af4467db973da8b9dc0167e
Tom@Pegasus <info@thomas-mundt.de>
2014-09-30 12:45:24 +0200

    excercise #11 finished

--- core/learnyounode/11-http-file-server.js
+++ core/learnyounode/11-http-file-server.js
@@ -4,6 +4,6 @@ var port = process.argv[2];
 var file = process.argv[3];
 
 var server = http.createServer(function(request, response) {
-	fs.createReadStream(file).pipe(response);
+	fs.createReadStream(file,{autoclose: true}).pipe(response);
 });
 server.listen(port);
